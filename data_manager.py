238ff98b2b7df2225eb6ef0f208be9a0941887fb
davidMolnar012 <davmmm@gmail.com>
2019-10-02 01:25:11 +0200

    major refactor

--- data_manager.py
+++ data_manager.py
@@ -1,13 +1,46 @@
 import database_common
+from psycopg2 import sql
 
 
 @database_common.connection_handler
-def get_latest_answers(cursor, limit=''):
-    if limit == '':
-        cursor.execute("""SELECT * FROM question ORDER BY submission_time DESC;""")
-    else:
-        cursor.execute("""SELECT * FROM question ORDER BY submission_time DESC
-         LIMIT %(limit)s  ;""", {'limit': limit})
+def select_sql(cursor, table, column='*', clause='', condition=[],
+               order_column='', order_asc_desc='', limit='', offset=''):
+    """"""
+    if len(condition) == 3:
+        condition[2] = '\'' + str(condition[2]) + '\''
+    cursor.execute(sql.SQL(
+        f'SELECT {column} FROM {table} '
+        f'{clause} {condition[0] + condition[1] + condition[2] if len(condition) == 3 else ""}'
+        f'{" ORDER BY " if order_column else ""}{order_column}{" " if order_asc_desc else ""}{order_asc_desc}'
+        f'{" LIMIT " if limit else ""}{limit}'
+        f'{" OFFSET " if offset else ""}{offset}'
+    ))
     return cursor.fetchall()
+
+
+@database_common.connection_handler
+def update_sql(cursor, table, column, update_value, update_condition=''):
+    """"""
+    cursor.execute(sql.SQL(f'UPDATE {table} SET {column} = \'{update_value}\' '
+                           f'{"WHERE " if update_condition else ""}{update_condition}'))
+
+
+@database_common.connection_handler
+def insert_record(cursor, table_name, records):
+    """"""
+    cursor.execute(sql.SQL(
+        f"INSERT INTO {table_name} ({', '.join(records.keys()).rstrip()}) " +
+        "VALUES ('{values}')".format(values='\', \''.join(records.values()).rstrip(', \''))))
+
+
+@database_common.connection_handler
+def get_table_head(cursor, table):
+    """"""
+    cursor.execute(sql.SQL("SELECT column_name FROM information_schema.columns WHERE table_name= '{table}' AND "
+                           "table_schema='public'".format(table=table)))
+    return [*[i['column_name'] for i in cursor.fetchall()]]
+
+
+
 
 
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+from time import gmtime, strftime
 
 import data_manager
 
@@ -6,16 +7,66 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def route_list1():
-    latest_5_answers = data_manager.get_latest_answers(5)
-    return render_template('index.html' , answers=latest_5_answers)
+def index():
+    latest_5_questions = data_manager.select_sql(
+        'question', order_column='submission_time', order_asc_desc='DESC', limit='5'
+    )
+    return render_template('index.html', questions=latest_5_questions)
 
 
 @app.route('/list')
-def route_list2():
-    answers = data_manager.get_latest_answers()
-    return render_template('index.html', answers=answers)
+def list_all():
+    questions = data_manager.select_sql(
+        'question', order_column='submission_time', order_asc_desc='DESC'
+    )
+    return render_template('list_all.html', questions=questions)
 
+
+@app.route('/question/<int:question_id>')
+def show_question(question_id):
+    question = data_manager.select_sql('question', clause='WHERE', condition=['id', '=', question_id])
+    answers = data_manager.select_sql('question', clause='WHERE', condition=['id', '=', question_id])
+    if not answers:
+        answers = [{'Answers': 'This question doesn\'t has any answer yet.'}]
+    question[0]['view_number'] += 1
+    data_manager.update_sql(
+        table='question', column='view_number',
+        update_value=question[0]['view_number'], update_condition=f'id={question_id}'
+    )
+    return render_template('display_question.html', question=question, answers=answers, question_id=question_id)
+
+
+@app.route('/add-question', methods=['GET', 'POST'])
+def add_question():
+    table_head = data_manager.get_table_head('question')
+    if request.method == 'POST':
+        new_record = {table_head[1]: str(strftime("%Y-%m-%d %H:%M:%S", gmtime())),
+                      table_head[2]: '0', table_head[3]: '0',
+                      table_head[4]: request.form[table_head[4]],
+                      table_head[5]: request.form[table_head[5]],
+                      table_head[6]: f'{request.form[table_head[6]] if request.form[table_head[6]] else None}'}
+        data_manager.insert_record('question', new_record)
+        new_record_id = data_manager.select_sql(
+            'question', column='id', clause='WHERE', condition=[table_head[1], '=', new_record[table_head[1]]]
+        )
+        return redirect(f'/question/{new_record_id[0]["id"]}')
+    return render_template('new_question.html', table_head=table_head)
+
+
+@app.route('/question/<int:question_id>/edit', methods=['GET', 'POST'])
+def edit_question(question_id):
+    table_head = data_manager.get_table_head('question')
+    question = data_manager.select_sql('question', clause='WHERE', condition=['id', '=', question_id])
+    if request.method == 'POST':
+        for column_name, element in request.form.items():
+            data_manager.update_sql('question', column_name, element, update_condition=f'id={question_id}')
+    return render_template('update_question.html', table_head=table_head, question=question, question_id=question_id)
+
+
+@app.route('/debug-url')
+def asdasd():
+    print(strftime("%Y-%m-%d %H:%M:%S.%F", gmtime()))
+    return redirect('/')
 
 
 if __name__ == '__main__':
